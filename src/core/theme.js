e94c6478ba8a9395eba3fab70ab008e6db299211
corpuzrayjhon <81072992+rambo223@users.noreply.github.com>
2021-05-28 23:38:32 +0800

    Initial commit

---
+++ src/core/theme.js
@@ -0,0 +1,12 @@
+import { DefaultTheme } from 'react-native-paper'
+
+export const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    text: 'skyblue',
+    primary: 'pink',
+    secondary: 'red',
+    error: '#f13a59',
+  },
+}
